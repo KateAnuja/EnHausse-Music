8382c2c723cb5128749f9685fcb8357ee375506c
Anuja Kate <kate.anuja15@gmail.com>
2021-04-24 20:38:16 +0530

    code fix

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -22,33 +22,3 @@ export class HomePage {
   }
 
 }
-
-
-
-import { HttpClient,HttpHeaders} from '@angular/common/http';
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class NetworkService {
-
-  constructor(public http : HttpClient) { }
-
-  sendRequest(videoId : any){
-    const httpOptions = {
-      headers: new HttpHeaders({
-        })
-      };
-      var body=`url=https://www.youtube.com/watch?v=${videoId}&q_auto=1&ajax=1`;
-
-      this.http.post("https://www.y2mate.com/mates/analyze/ajax",body, httpOptions)
-      .subscribe(data => {
-        console.log(data);
-       }, error => {
-        console.log(error);
-      });
-      
-  }
-}
-
