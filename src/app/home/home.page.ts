9712760ac1439d1e38820298d35b779fb38e2319
Anuja Kate <kate.anuja15@gmail.com>
2021-04-24 20:45:14 +0530

    intendation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,15 +10,15 @@ export class HomePage {
   url : string="https://www.youtube.com/watch?v=mt9xg0mmt28";
   constructor() {}
 
-  onClick(){
-    console.log(this.url);
-    var videoid = this.url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
-    if(videoid != null) {
-    console.log("video id = ",videoid[1]);
-    // this.networkService.sendRequest(videoid);
-    } else { 
-    console.log("The youtube url is not valid.");
-   }
+  verifyUrl(){
+    let videoid = this.url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+    if(videoid != null){
+      console.log("video id = ",videoid[1]);
+      
+    }else{ 
+      //TODO: add alert for non youtube url 
+      console.log("The youtube url is not valid.");
+    }
   }
 
 }
