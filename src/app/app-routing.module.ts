2970707d41a4e02af3fc4ad572259510988368c8
Anuja Kate <kate.anuja15@gmail.com>
2021-04-26 23:07:24 +0530

    search and suggestion from youtube

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,12 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'search',
     pathMatch: 'full'
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
   },
 ];
 
