ba714410f8cac06a565186a09cb85c41beb0a6ef
Anuja Kate <kate.anuja15@gmail.com>
2021-05-04 02:14:15 +0530

    player Fn <-> UI

--- src/app/floating-music-player/floating-music-player.component.ts
+++ src/app/floating-music-player/floating-music-player.component.ts
@@ -24,103 +24,107 @@ export class FloatingMusicPlayerComponent {
   prevMusicTrack:MusicTrack;
   totalDurationOfPlaylist:number=0;
   isPlaying : boolean = false;
-  orderPreference = MusicPlayerOrderPreference;
+  orderPreferenceRef = MusicPlayerOrderPreference;
+  orderPreference : MusicPlayerOrderPreference= MusicPlayerOrderPreference.ALL_IN_LOOP;
   audioFile : MediaObject;
   audioProgressBarInterval=null;
   audioDuration=-1;
   isSeekBarFocused=false;
+  soudTrackName="Music Track"
 
 
   
   constructor(
-    //private musicTrackService:MusicTrackService,
-    //private media : Media,
-    //private platform : Platform,
+    private musicTrackService:MusicTrackService,
+    private media : Media,
+    private platform : Platform,
   ) { 
-    // this.musicTrackService.playerDataBehaviorSubject
-    //   .subscribe(this.initMusicPlayer.bind(this));
+    this.musicTrackService.playerDataBehaviorSubject
+      .subscribe(this.initMusicPlayer.bind(this));
   }
 
-  // OnSeekTrackFocus(){
-  //   this.isSeekBarFocused=true;
-  // }
+  OnSeekTrackFocus(){
+    this.isSeekBarFocused=true;
+  }
 
-  // initMusicPlayer(mP:MusicPlayer){
-  //   if(mP){
-  //     this.audioDuration=-1;
-  //     if(this.audioProgressBarInterval){
-  //       clearInterval(this.audioProgressBarInterval);
-  //     }
-  //     this.currentMusicTrack=mP.currentMusictTrack;
-  //     this.musicTrackArray=mP.musicTrackArray;
-  //     MusicPlayerUtil.startPlayer(
-  //                           mP.currentMusictTrack,
-  //                           mP.musicTrackArray, 
-  //                           mP.orderPreference
-  //                     );
-  //     this.nextMusicTrack=MusicPlayerUtil.getNext();
-  //     this.prevMusicTrack=MusicPlayerUtil.getPrev();
-  //     this.totalDurationOfPlaylist=MusicPlayerUtil.getTotalDuration();
-  //     if(this.audioFile){
-  //       this.audioFile.stop();
-  //     }
-  //     this.audioFile = this.media.create(
-  //       this.currentMusicTrack.path.replace(/^file:\/\//,'')
-  //     );
-  //     this.audioFile.play();
+  initMusicPlayer(mP:MusicPlayer){
+    if(mP){
+      this.audioDuration=-1;
+      if(this.audioProgressBarInterval){
+        clearInterval(this.audioProgressBarInterval);
+      }
+      this.currentMusicTrack=mP.currentMusictTrack;
+      this.musicTrackArray=mP.musicTrackArray;
+      this.orderPreference=mP.orderPreference;
+      MusicPlayerUtil.startPlayer(
+                            mP.currentMusictTrack,
+                            mP.musicTrackArray, 
+                            mP.orderPreference
+                      );
+      this.nextMusicTrack=MusicPlayerUtil.getNext();
+      this.prevMusicTrack=MusicPlayerUtil.getPrev();
+      this.totalDurationOfPlaylist=MusicPlayerUtil.getTotalDuration();
+      if(this.audioFile){
+        this.audioFile.stop();
+      }
+      this.audioFile = this.media.create(
+        this.currentMusicTrack.path.replace(/^file:\/\//,'')
+      );
+      this.audioFile.play();
       
-  //     this.audioProgressBarInterval=setInterval(async ()=>{
-  //       this.audioFile.getCurrentPosition().then((pos)=>{
-  //         if(!this.isSeekBarFocused){
-  //           if(this.audioDuration<0){
-  //             this.audioDuration=this.audioFile.getDuration()*1000;
-  //           }
-  //           console.log(Number(this.trackRange.value))
-  //           this.trackRange.value=(pos*1000*100/this.audioDuration)
-  //           if(Number(this.trackRange.value)==100 || Number(this.trackRange.value)<0){
-  //             this.next();
-  //           }
-  //         }
-  //       })
-  //     },200);
-  //     this.isPlaying=true;
-  //   }
-  // }
+      this.audioProgressBarInterval=setInterval(async ()=>{
+        this.audioFile.getCurrentPosition().then((pos)=>{
+          if(!this.isSeekBarFocused){
+            if(this.audioDuration<0){
+              this.audioDuration=this.audioFile.getDuration()*1000;
+            }
+            this.trackRange.value=(pos*1000*100/this.audioDuration)
+            if(Number(this.trackRange.value)==100 || Number(this.trackRange.value)<0){
+              this.next();
+            }
+          }
+        })
+      },200);
+      this.isPlaying=true;
+    }
+  }
 
-  // prev(){
-  //   this.audioFile.stop();
-  //   this.musicTrackService.playTrack(this.prevMusicTrack,this.musicTrackArray);
-  // }
+  prev(){
+    this.audioFile.stop();
+    this.musicTrackService.playTrack(this.prevMusicTrack,this.musicTrackArray);
+  }
 
-  // next(){
-  //   this.audioFile.stop();
-  //   this.musicTrackService.playTrack(this.nextMusicTrack,this.musicTrackArray);
-  // }
+  next(){
+    this.audioFile.stop();
+    this.musicTrackService.playTrack(this.nextMusicTrack,this.musicTrackArray);
+  }
 
-  // togglePlayer(pause){
-  //   this.isPlaying=!this.isPlaying;
-  //   if(!this.isPlaying){
-  //     this.audioFile.pause();
-  //   }else{
-  //     this.audioFile.play();
-  //   }
-  // }
+  togglePlayer(){
+    this.isPlaying=!this.isPlaying;
+    if(!this.isPlaying){
+      this.audioFile.pause();
+    }else{
+      this.audioFile.play();
+    }
+  }
 
-  // setOrderPreference(orderPreference : MusicPlayerOrderPreference){
-  //   let adjacentTrackObj=this.musicTrackService
-  //                         .setMusicOrderPreference(orderPreference);
-  //   this.prevMusicTrack=adjacentTrackObj.prevTrack;
-  //   this.nextMusicTrack=adjacentTrackObj.nextTrack;
-  // }
+  setOrderPreference(orderPreference : MusicPlayerOrderPreference){
+    this.orderPreference=orderPreference;
+    let adjacentTrackObj=this.musicTrackService
+                          .setMusicOrderPreference(orderPreference);
+    
+    this.prevMusicTrack=adjacentTrackObj.prevTrack;
+    this.nextMusicTrack=adjacentTrackObj.nextTrack;
+  }
 
-  // seekTrackTo(){
-  //   let duration=this.audioDuration;
-  //   let seekToVal=1;
-  //   if(Number(this.trackRange.value)){
-  //     seekToVal=duration*(Number(this.trackRange.value)/100)
-  //   }
-  //   this.audioFile.seekTo(seekToVal);
-  //   this.isSeekBarFocused=false;
-  // }
+  seekTrackTo(){
+    let duration=this.audioDuration;
+    let seekToVal=1;
+    if(Number(this.trackRange.value)){
+      seekToVal=duration*(Number(this.trackRange.value)/100)
+    }
+    this.audioFile.seekTo(seekToVal);
+    this.isSeekBarFocused=false;
+  }
 
 }
